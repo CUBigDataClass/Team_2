7fc878572792ea955ee34fbd040f701d15aef2e1
soham-shah <sohamshah225@gmail.com>
2016-04-12 21:45:34 -0600

    added basic title for view
    setting django to direct to the profiles view

---
+++ website/profiles/urls.py
@@ -0,0 +1,9 @@
+__author__ = 'soham'
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    #url(r'^sanders/', admin.site.urls)
+]
