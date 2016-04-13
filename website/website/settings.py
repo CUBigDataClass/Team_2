0072f9260e5ca822c37777f22734d6eed8e0e0ec
soham-shah <sohamshah225@gmail.com>
2016-04-12 22:05:02 -0600

    update settings.py
    added profiles to settings and update the database

--- website/website/settings.py
+++ website/website/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
