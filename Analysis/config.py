d4602617955040cb51e11a908789820ed95aeba5
evbr1432 <evbr1432@colorado.edu>
2016-04-17 02:41:43 -0600

    Ignoring config file worked. Go ahead and place creds for oauth in config.py. Also cleaned some stuff up

--- Analysis/config.py
+++ Analysis/config.py
@@ -1,4 +1,4 @@
 consumer_key = 'Going to push basic config then add it to git ignore'# remove this before git push
-consumer_secret = '' # remove this before git push
+consumer_secret = 'if this does not show then git ignored this file' 
 access_token = '' # remove this before git push
 access_token_secret = '' # remove this before git push
