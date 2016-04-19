8d78da9692c554711fa34edd43c8ffcdf1132802
soham-shah <sohamshah225@gmail.com>
2016-04-19 15:57:52 -0600

    add a basic template
    it’s in views.py

--- website/profiles/views.py
+++ website/profiles/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("<h1> This is the profile for a candidate")
 
-def sanders(request):
+def cadidate(request):
     template = loader.get_template('profiles/index.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
