b591c833983cc0ab280c6d7f1df8fbaa39a867d5
EC2 Default User <ec2-user@ip-172-31-17-222.us-west-2.compute.internal>
2016-04-20 22:55:43 +0000

    adding average sentiments

--- SparkTwitterHelloWorldExample/src/main/java/de/michaelgoettsche/SparkTwitterHelloWorldExample.java
+++ SparkTwitterHelloWorldExample/src/main/java/de/michaelgoettsche/SparkTwitterHelloWorldExample.java
@@ -12,6 +12,8 @@ import twitter4j.*;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.util.Properties;
+import java.io.PrintStream;
+import java.io.OutputStream;
 import java.io.IOException;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
@@ -31,29 +33,33 @@ import edu.stanford.nlp.util.CoreMap;
 public class SparkTwitterHelloWorldExample {
 	public static int findSentiment(String line) {
 		int mainSentiment = 0; 
-		
+		PrintStream err = System.err;
+		System.setErr(new PrintStream(new OutputStream() {
+    			public void write(int b) {
+    			}
+		}));	
 		//set up properties
 		Properties props = new Properties();
 		props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
 		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 		//estimate the mood score of each tweet
 		if (line != null && line.length() > 0) {
-		                                		                    int longest = 0;
-		                                		                                Annotation annotation = pipeline.process(line);
-		                                		                                            for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
-		                                		                                                            Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
-		                                		                                                                            int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-		                                		                                                                                            String partText = sentence.toString();
-		                                		                                                                                                            if (partText.length() > longest) {
-		                                		                                                                                                                                mainSentiment = sentiment;
-		                                		                                                                                                                                                    longest = partText.length();
-		                                		                                                                                                                                                                    }
-		                                		                                                                                                                                                                                }
-		                                		                                                                                                                                                                                        }
-		                                		                                                                                                                                                                                                
-		                                		                                                                                                                                                                                                        return mainSentiment;
-		                                		                                                                                                                                                                                                         
-		                                		                                                                                                                                                                                                             }
+                int longest = 0;
+          	Annotation annotation = pipeline.process(line);
+		for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+			Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
+		        int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+		        String partText = sentence.toString();
+		        if (partText.length() > longest) {
+		        	mainSentiment = sentiment;
+		                longest = partText.length();
+		        }
+		}
+	}
+System.setErr(err);         
+return mainSentiment;
+
+}
     public static void main(String[] args) {
         try{
             Scanner in = new Scanner(new FileReader("/home/ec2-user/tokens.txt"));
@@ -80,23 +86,128 @@ public class SparkTwitterHelloWorldExample {
         // Without filter: Output text of all tweets
         JavaDStream<String> statuses = twitterStream.map(
                 new Function<Status, String>() {
-			public String call(Status status) { int score = findSentiment(status.getText());
-return status.getText()+" Score: " + score; }
+			public String call(Status status) { return status.getText(); }
                 }
         );
-	/*JavaDStream<String> words = statuses.flatMap(
+	JavaDStream<String> words = statuses.flatMap(
 		new FlatMapFunction<String, String>(){
 			public Iterable<String> call(String in) {
 				return Arrays.asList(in.split(" "));
 			}
 		}
 	);
-	
+	/*
 	JavaDStream<String> hashTags = words.filter(
 		new Function<String, Boolean>() {
 			public Boolean call(String word) { return  word.startsWith("#");}
 		}
 	);
+	*/
+	JavaDStream<String> trumptweets = statuses.filter(
+		new Function<String, Boolean>() {
+			public Boolean call(String word) {return word.toLowerCase().contains("trump") & !word.toLowerCase().contains("hillary") & !word.toLowerCase().contains("bernie") & !word.toLowerCase().contains("cruz");}
+		}
+	  );
+	JavaPairDStream<String, Integer> trumptuples = trumptweets.mapToPair(
+		new PairFunction<String, String, Integer>(){
+			public Tuple2<String, Integer> call(String in) {
+				return new Tuple2<String, Integer>("trump", findSentiment(in));
+			}
+		}
+	);
+	JavaPairDStream<String, Integer> trumpcounts = trumptuples.reduceByKey(
+      		new Function2<Integer, Integer, Integer>() {
+        		public Integer call(Integer i1, Integer i2) { return i1 + i2; }
+      }
+);
+
+		JavaDStream<String> hillarytweets = statuses.filter(
+			new Function<String, Boolean>() {
+				public Boolean call(String word) {return !word.toLowerCase().contains("trump") & (word.toLowerCase().contains("hillary") || word.toLowerCase().contains("clinton")) & !word.toLowerCase().contains("bernie") & !word.toLowerCase().contains("cruz");}
+		}
+	  );	
+		JavaPairDStream<String, Integer> hillarytuples = hillarytweets.mapToPair(
+			new PairFunction<String, String, Integer>(){
+			public Tuple2<String, Integer> call(String in) {
+				return new Tuple2<String, Integer>("hillary", findSentiment(in));
+			}
+		}
+	);
+	JavaPairDStream<String, Integer> hillarycounts = hillarytuples.reduceByKey(
+      		new Function2<Integer, Integer, Integer>() {
+        		public Integer call(Integer i1, Integer i2) { return i1 + i2; }
+      }
+);
+		JavaDStream<String> bernietweets = statuses.filter(
+		new Function<String, Boolean>() {
+			public Boolean call(String word) {return !word.toLowerCase().contains("trump") & !word.toLowerCase().contains("hillary") & (word.toLowerCase().contains("bernie") || word.toLowerCase().contains("sanders")) & !word.toLowerCase().contains("cruz");}
+		}
+	  );	JavaPairDStream<String, Integer> bernietuples = bernietweets.mapToPair(
+		new PairFunction<String, String, Integer>(){
+			public Tuple2<String, Integer> call(String in) {
+				return new Tuple2<String, Integer>("bernie", findSentiment(in));
+			}
+		}
+	);
+	JavaPairDStream<String, Integer> berniecounts = bernietuples.reduceByKey(
+      		new Function2<Integer, Integer, Integer>() {
+        		public Integer call(Integer i1, Integer i2) { return i1 + i2; }
+      }
+);
+			JavaDStream<String> cruztweets = statuses.filter(
+		new Function<String, Boolean>() {
+			public Boolean call(String word) {return !word.toLowerCase().contains("trump") & !word.toLowerCase().contains("hillary") & !word.toLowerCase().contains("bernie") & word.toLowerCase().contains("cruz");}
+		}
+	  );	JavaPairDStream<String, Integer> cruztuples = cruztweets.mapToPair(
+		new PairFunction<String, String, Integer>(){
+			public Tuple2<String, Integer> call(String in) {
+				return new Tuple2<String, Integer>("cruz", findSentiment(in));
+			}
+		}
+	);
+		
+	JavaPairDStream<String, Integer> cruzcounts = cruztuples.reduceByKey(
+      		new Function2<Integer, Integer, Integer>() {
+        		public Integer call(Integer i1, Integer i2) { return i1 + i2; }
+      }
+);
+		
+	JavaDStream<String> demtweets =  statuses.filter(
+		new Function<String, Boolean>() {
+			public Boolean call(String word) {return word.toLowerCase().contains("democrat") & !word.toLowerCase().contains("republican");}
+		}
+	  );
+	JavaPairDStream<String, Integer> demtuples = demtweets.mapToPair(
+		new PairFunction<String, String, Integer>(){
+			public Tuple2<String, Integer> call(String in) {
+				return new Tuple2<String, Integer>("dems", findSentiment(in));
+			}
+		}
+	);
+
+	JavaPairDStream<String, Integer> demcounts = demtuples.reduceByKey(
+      		new Function2<Integer, Integer, Integer>() {
+        		public Integer call(Integer i1, Integer i2) { return i1 + i2; }
+      }
+);
+	JavaDStream<String> goptweets =  statuses.filter(
+		new Function<String, Boolean>() {
+			public Boolean call(String word) {return !word.toLowerCase().contains("democrat") & word.toLowerCase().contains("republican") & word.toLowerCase().contains("gop");}
+		}
+	  );
+		JavaPairDStream<String, Integer> goptuples = goptweets.mapToPair(
+		new PairFunction<String, String, Integer>(){
+			public Tuple2<String, Integer> call(String in) {
+				return new Tuple2<String, Integer>("gop", findSentiment(in));
+			}
+		}
+	);
+		JavaPairDStream<String, Integer> gopcounts = goptuples.reduceByKey(
+      		new Function2<Integer, Integer, Integer>() {
+        		public Integer call(Integer i1, Integer i2) { return i1 + i2; }
+      }
+);		
+	/*
 	String wordSentimentFilePath = "AFINN/AFINN-111.txt";
 	final JavaPairRDD<String, Double> wordSentiments = jssc.sparkContext().textFile(wordSentimentFilePath).mapToPair(new PairFunction<String, String, Double>(){
 		public Tuple2<String, Double> call(String line) {
@@ -143,7 +254,12 @@ return status.getText()+" Score: " + score; }
                 }
         );
 */
-        statuses.print();
+	trumpcounts.print();
+        hillarycounts.print();
+	berniecounts.print();
+	cruzcounts.print();
+	gopcounts.print();
+	demcounts.print();
 	jssc.start();
 	} 
 	catch(IOException e){
