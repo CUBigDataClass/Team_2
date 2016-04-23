fe08d01c7d606ffb83623b6aa916874383174f56
soham-shah <sohamshah225@gmail.com>
2016-04-23 13:39:37 -0600

    Updating the website
    - Combine old elements.
    - delete both django folders

--- website/profiles/urls.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'soham'
-
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^sanders/$', views.index, name='sanders')
-]
